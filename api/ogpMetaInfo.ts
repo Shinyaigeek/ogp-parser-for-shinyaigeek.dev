96955c951a1297c97f55c65d376520acc988262c
Shinyaigeek <Philispaxil@gmail.com>
2021-10-19 17:29:46 +0900

    Feature: init vercel functions api

---
+++ api/ogpMetaInfo.ts
@@ -0,0 +1,6 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default async (request: VercelRequest, response: VercelResponse) => {
+  const items = [{ asdf: "hoge" }];
+  response.status(200).json(items);
+};
