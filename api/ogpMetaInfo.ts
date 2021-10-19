f41a98310ed4e98a451155420eb908a321b29705
Shinyaigeek <Philispaxil@gmail.com>
2021-10-19 17:44:05 +0900

    Feature: get target url from query parameter

--- api/ogpMetaInfo.ts
+++ api/ogpMetaInfo.ts
@@ -2,6 +2,13 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 import ogpParser from "ogp-parser";
 
 export default async (request: VercelRequest, response: VercelResponse) => {
-  const ogp = await ogpParser("https://shinyaigeek.dev/");
+  const validateUrl = (url: string | string[]): string => {
+    if (typeof url === "string") {
+      return url;
+    }
+
+    return url[0];
+  };
+  const ogp = await ogpParser(validateUrl(request.query.url));
   response.status(200).json(ogp);
 };
